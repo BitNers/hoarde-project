1c68c2e4dcae3b534878301b4778b7a4dd48cca2
BitNers <wesleyalvessilva@outlook.com.br>
2022-12-06 15:33:18 -0300

    Update index route
    Adding Login and Register for users.

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,35 @@
 var express = require('express');
 var router = express.Router();
+const UAC_Controller = require('../controllers/UAC_UserController');
+const passport = require("passport");
 require('dotenv');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: process.env.APP_NAME });
 });
+
+router.get('/login', function(req, res, next) {
+  res.render('login', { title: process.env.APP_NAME });
+});
+
+
+router.get('/register', function(req, res, next) {
+  res.render('register', { title: process.env.APP_NAME });
+});
+
+
+router.post("/register", function(req,res,next){
+  UAC_Controller.register_new_user(req,res);
+});
+
+
+router.post("/login", passport.authenticate("local-login", 
+  { 
+    failureRedirect:"/login", 
+    successRedirect: "/user"
+  })
+);
+
 
 module.exports = router;
