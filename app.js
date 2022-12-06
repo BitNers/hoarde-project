10e4794f0b4cbd62f6fb5c9225fcdbe0c2c47bf3
BitNers <wesleyalvessilva@outlook.com.br>
2022-12-05 22:29:59 -0300

    Update app core
    Adding new libraries. Adding new routes. Changing session settings.

--- app.js
+++ app.js
@@ -1,26 +1,69 @@
-var createError = require('http-errors');
+// Call all required libraries
+
 var express = require('express');
+var createError = require('http-errors');
 var path = require('path');
 var cookieParser = require('cookie-parser');
+var cookieSession = require('cookie-session');
+
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
+
+// Create Application Express
+
+/*
+    Security and Performance
+*/
+var helmet = require("helmet");
+var cors = require("cors");
+var compression = require('compression');
+var passpt = require('./config/passport');
+
+/*
+    ROUTES
+*/
+var indexRouter = require('./routes/index');
+var apiRouter = require("./routes/api");
+
+
+
+require('dotenv')
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'pug');
 
+
+/*
+  MAINTENANCE MODE
+*/
+// if(process.env.MAINTENANCE_MODE == "TRUE"){app.use(require('./controllers/middlewares/middleMaintenance'));}
+
+app.use(cors({credentials: true}))
+app.use(helmet());
+app.use(compression({level:9})) // Organizar isso.
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+app.use(cookieSession({
+  secret: process.env.APP_TOKEN_SESSION,
+  resave: false,
+  saveUninitialized: false,
+  maxAge: 1 * (60 * 1000) // 1 min.
+})) 
+
+app.use(passpt.initialize());
+app.use(passpt.session());
+
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/api', apiRouter);
+// app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
