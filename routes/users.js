48e85e2c30a4dd1072dcb96be805e386184d613f
BitNers <wesleyalvessilva@outlook.com.br>
2022-12-06 15:33:02 -0300

    Updating User Router
    User Route for Dashboard and validate authentication and LogOut.

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,24 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  if(!req.isAuthenticated()) res.redirect("/")
+  
+  res.redirect("/user/dashboard");
+
 });
+
+
+router.get('/dashboard', function (req,res,next){
+  if(!req.isAuthenticated()) res.redirect("/")
+
+    res.render("dashboard/index");
+}
+);
+
+
+router.post('/logout', function(req,res,next){
+  req.logout();
+  res.redirect("/");
+})
 
 module.exports = router;
