a6b7a8d55d172ab8a47c63beede679be9db6ad3f
BitNers <wesleyalvessilva@outlook.com.br>
2022-12-06 15:35:08 -0300

    Update Passport Config
    Validating Login User.

--- config/passport.js
+++ config/passport.js
@@ -2,42 +2,53 @@ const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
 const bcrypt = require('bcrypt');
 const {UACModel}  = require('../config/database');
+const {Op} = require("sequelize");
 
 passport.serializeUser(function(user, done) {
-    delete user.password;
+    delete user.Password;
     done(null, user);
   });
   
   passport.deserializeUser(function(user, done) {
-    delete user.password;
+    delete user.Password;
     done(null, user);
   });
 
 
 passport.use('local-login', new LocalStrategy({
-    usernameField: 'user_name',
-    passwordField: 'password'
-},function verify(user_name, password, cb){
-
-    UACModel.findOne(
-            {attributes:['user_name', 'password', 'document_number'], 
-            where: {user_name: user_name}})
-    .then(user=>{
-
-        if(!user){return cb(null, false, {message: "Incorrect password or username."})}
+    usernameField: 'Username',
+    passwordField: 'Password'
+},function verify(Username, Password, cb){
     
-        if(bcrypt.compareSync((process.env.PRESALT_FOR_PASSWORD + password + process.env.POSSALT_FOR_PASSWORD ), user.password)){
-            return cb(null, user);
-        }else{
-            return cb(null, false, {message: "Incorrect password or username."})
-        }
-    
-    })
-    .catch(err=>{
-        console.error(`[X] Error while reading data from SQL.\t ${err.message}`);
-        return cb(null, false, {message: "Error while logging"});
-    })
-    ;
+    UACModel.findOne({
+        where: {
+            [Op.or]:[
+                {Username: Username},
+                {Email: Username}
+            ]
+        },
+        attributes: ['UserID', 'Username', 'Email', 'Password', 'PasswordSalt']
+    }).then(user => {
+
+        if(!user) return cb(null, false, {"message": "Incorrect Password or Username."});
+            
+        bcrypt.compare(Password + user["PasswordSalt"], user["Password"])
+
+            .then(validateLogin =>{
+
+                if(!validateLogin) return cb(null, false, {"message": "Wrong credentials."});
+
+                return cb(null, user);
+
+        }).catch(err =>{
+            console.error(`[X] Error while reading data from SQL.\t ${err.message}`);
+            return cb(null, false, {"message": "User not found."});
+        });
+
+        
+
+    });
+
 }))
 
 
